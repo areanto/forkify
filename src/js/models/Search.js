613f4811da6996fbc4034057feab487d70663eef
Areanto <ferdiareanto@gmail.com>
2018-06-17 18:44:54 +0700

    Add Search Model

---
+++ src/js/models/Search.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResults() {
+        const proxy = "https://cors-anywhere.herokuapp.com/";
+        const key = "da07beb1527fc5a305ed0862c19cc2d1";
+        const url = "http://food2fork.com/api/search";
+
+        try {
+            const res = await axios(`${proxy}${url}?key=${key}&q=${this.query}`);
+            this.result = res.data.recipes;
+            console.log(this.result);
+        } catch (error) {
+            alert(error);
+        }
+    }
+}
