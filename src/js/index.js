85513c2cbdfbd6f8d371a89f11cd219727ddcd21
Areanto <ferdiareanto@gmail.com>
2018-06-17 08:45:31 +0700

    Setup API call using axios
    npm install axios --save

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,18 @@
-// Global app controller
+import axios from "axios";
+
+async function getResults(query) {
+    const proxy = "https://cors-anywhere.herokuapp.com/";
+    const key = "da07beb1527fc5a305ed0862c19cc2d1";
+    const url = "http://food2fork.com/api/search";
+
+    try{
+        const res = await axios(`${proxy}${url}?key=${key}&q=${query}`);
+        const recipes = res.data.recipes;
+        console.log(recipes);
+    } 
+    catch(error){
+        alert(error);
+    }
+}
+
+getResults("quiche");
